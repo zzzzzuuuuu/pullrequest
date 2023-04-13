13e5d567dd388c10cc5521fa560bcddcf6eb71ca
yhyem <yoonhyemin0120@gmail.com>
2023-04-14 03:32:13 +0900

    chore: 파일 초기 세팅

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,15 +1,7 @@
 import "../css/Main.css";
 
 const Main = (props) => {
-  const { name, age, hobby } = props.data;
-
-  return (
-    <div>
-      {name}
-      {age}
-      {hobby}
-    </div>
-  );
+  return <></>;
 };
 
 export default Main;
